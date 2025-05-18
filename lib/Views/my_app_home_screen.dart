ad9390ec4566fb21786448fd093973d6d54b850c
Joan-Mena <toniomexico8@gmail.com>
2025-05-17 19:27:36 -0600

    PrimerCommit

---
+++ lib/Views/my_app_home_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/Utils/constants.dart';
+import 'package:flutter_firebase/Widget/banner.dart';
+import 'package:flutter_firebase/Widget/my_icon_button.dart';
+import 'package:iconsax/iconsax.dart';
+
+class MyAppHomeScreen extends StatefulWidget {
+  const MyAppHomeScreen({super.key});
+
+  @override
+  State<MyAppHomeScreen> createState() => _MyAppHomeScreenState();
+}
+
+class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: KbackgroundColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  children: [
+                    headerParts(),
+                    mySearchBar(),
+                    //para el baner
+                    BannerToExplore()
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding mySearchBar() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: const Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipes",
+          hintStyle: const TextStyle(
+            color: Colors.grey,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row headerParts() {
+    return Row(
+      children: [
+        Text(
+          "Que estas \ncocinando hoy?",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            height: 1,
+          ),
+        ),
+        const Spacer(),
+        MyIconButton(icon: Iconsax.notification, pressed: () {})
+      ],
+    );
+  }
+}
